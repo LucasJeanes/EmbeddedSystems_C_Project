20e7b34fe8faeba2bcc512de4fcbb08abf0dcd2a
LUCAS JEANES <G00410024@atu.ie>
2025-02-20 16:16:57 +0000

    First upload of project files

---
+++ Core/Inc/WiFi_Credentials.h
@@ -0,0 +1,14 @@
+/*
+ * WiFi_Credentials.h
+ *
+ *  Created on: 16 Oct 2022
+ *      Author: nokeeffe
+ */
+
+#ifndef INC_WIFI_CREDENTIALS_H_
+#define INC_WIFI_CREDENTIALS_H_
+
+#define SSID     "Homebrew Mobile"
+#define PASSWORD "lucas2009"
+
+#endif /* INC_WIFI_CREDENTIALS_H_ */
